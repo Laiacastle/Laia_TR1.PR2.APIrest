9fdc0bec0d9734e6e5f157545a34dcfb2be72cb0
Laiacastle <laia.castillo.7e6@itb.cat>
2025-04-22 01:08:02 +0200

    feat(api): Afegir Xat

---
+++ Api_Laia_T1.PR2.APIrest/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Api_Laia_T1.PR2.APIrest.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //metode invocable desde el client
+        public async Task SendMessage(string user, string message)
+        {
+            //envia el missatge a tots el s clients
+            await Clients.All.SendAsync("ReciveMessage", user, message);
+        }
+    }
+}
