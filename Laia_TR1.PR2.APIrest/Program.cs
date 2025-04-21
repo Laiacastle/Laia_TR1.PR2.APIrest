478eaa163d130ac1a195568450819a66f0050eb9
Laiacastle <laia.castillo.7e6@itb.cat>
2025-04-21 23:52:59 +0200

    Add project files.

---
+++ Laia_TR1.PR2.APIrest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Laia_TR1.PR2.APIrest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
