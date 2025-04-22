7cf4f37b26704060757a6b08b2fe3c705d1534bd
Laiacastle <laia.castillo.7e6@itb.cat>
2025-04-22 03:50:22 +0200

    feat(client): Afegit el registre d'usuaris i d'administradors

---
+++ Client_Laia_T1.PR2.APIrest/Pages/Register.cshtml.cs
@@ -0,0 +1,51 @@
+using Api_Laia_T1.PR2.APIrest.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client_Laia_T1.PR2.APIrest.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ILogger _logger;
+
+        [BindProperty]
+        public RegisterDTO Register { get; set; } = new RegisterDTO();
+        public string? ErrorMessage { get; set; }
+
+        public RegisterModel(ILogger<RegisterModel> logging)
+        {
+            _logger = logging;
+        }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7062/api/Authentication/");
+                    var response = await client.PostAsJsonAsync("register", Register);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        _logger.LogInformation("Register failed");
+                        ErrorMessage = "Error en alguna credencial o usuari ja registrat.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error durant el registre");
+                ErrorMessage = "Error inesperat. Torna-ho a intentar.";
+            }
+
+            return RedirectToPage("Login");
+        }
+    }
+}
+
