8951fb23ac7f6eca1064aba27371152567cc6bbd
Laiacastle <laia.castillo.7e6@itb.cat>
2025-04-22 02:34:42 +0200

    feat(client): Afegida la pagina principal amb els jocs + configuracio de connexio client - api

--- Client_Laia_T1.PR2.APIrest/Program.cs
+++ Client_Laia_T1.PR2.APIrest/Program.cs
@@ -1,14 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
 namespace Client_Laia_T1.PR2.APIrest
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //hacemos que espera para que se ejecute primero la api
+            await Task.Delay(3000);
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            //damos accesa a la configuración de appsettings
+            string urlApi = builder.Configuration["ApiSettings:BaseUrl"] ?? throw new InvalidOperationException("Api base URL not found");
+
+            //añadimos la connexion HttpClient
+            builder.Services.AddHttpClient("ApiLaia", client =>
+            {
+                client.BaseAddress = new Uri(urlApi);
+
+            });
+
+            builder.Services.AddHttpClient("AuthorizedClient", client =>
+            {
+                client.BaseAddress = new Uri(urlApi);
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            });
+            //
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = "/Login";
+                options.AccessDeniedPath = "/AccesDenied";
+            });
+
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddHttpClient<AuthenticationService>(client =>
+            {
+                client.BaseAddress = new Uri(urlApi);
+            });
+
+            builder.Services.AddSession();
+
+            builder.Services.AddSession();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -20,6 +57,8 @@ namespace Client_Laia_T1.PR2.APIrest
             }
 
             app.UseHttpsRedirection();
+            //Activamos la session
+            app.UseSession();
 
             app.UseRouting();
 
@@ -28,6 +67,11 @@ namespace Client_Laia_T1.PR2.APIrest
             app.MapStaticAssets();
             app.MapRazorPages()
                .WithStaticAssets();
+            app.MapGet("/", context =>
+            {
+                context.Response.Redirect("/ViewGames");
+                return Task.CompletedTask;
+            });
 
             app.Run();
         }
